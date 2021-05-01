ec4be1dbeb79aed623513c7be3aff09d20c11c08
royersynopsis <royerleandroroblesvega@gmail.com>
2021-05-01 10:37:26 -0500

    Changes test

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ sys.path.insert(0, os.path.dirname(__file__))
 
 def app(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    message = 'It works!\n'
+    message = 'It works1111!\n'
     version = 'Python v' + sys.version.split()[0] + '\n'
     response = '\n'.join([message, version])
     return [response.encode()]
