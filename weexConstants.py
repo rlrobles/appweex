2d7a2c5464efedd48e477ea859539e440020bb1c
royersynopsis <royerleandroroblesvega@gmail.com>
2021-05-02 20:55:20 -0500

    change params

--- weexConstants.py
+++ weexConstants.py
@@ -1,13 +1,14 @@
 
-"""
+
 MYSQL_HOST = 'localhost'
 MYSQL_USER = 'weex_admirasite'
 MYSQL_PASSWORD = 'weex_admirasite123.'
 MYSQL_DB = 'weex_admira' 
 
-"""
 
+"""
 MYSQL_HOST = 'localhost'
 MYSQL_USER = 'root'
 MYSQL_PASSWORD = 'admin'
 MYSQL_DB = 'weex_admira'
+"""
