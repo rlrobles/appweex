4887efeea9d7a50d18b066d1ad524f4278130fd8
royersynopsis <royerleandroroblesvega@gmail.com>
2021-05-06 23:59:54 -0500

    ffffffffffffff

--- job.py
+++ job.py
@@ -1,5 +1,11 @@
+
+import requests
+
 def sensor():
     """ Function for test purposes. """
+    r = requests.get('http://demo.weex.pe/home')
+    r.status_code
+    print(r.status_code)
     print("Scheduler is alive!")
 
 sensor()
