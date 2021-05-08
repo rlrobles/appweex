054cac27f85a755f0a6acb494806c4d707627366
Jordan Ocmin <jocmin@proveedor.intercorp.com.pe>
2021-05-07 19:01:44 -0500

    XD

--- app.py
+++ app.py
@@ -153,7 +153,7 @@ def apiTipoCambioMonedas():
     ##jobUpdateTipoCambio()
     return jsonify(result)
 
-@app.route('/weex/tasa-cambio/v1/<moneda>', methods=['POST'])
+@app.route('/weex/tasa-cambio/v1/<moneda>', methods=['GET'])
 def apiTipoCambioMonedashome(moneda):
     cur = mysql.connection.cursor()
     cur.execute("SELECT COMPRA, VENTA FROM tasa_cambio WHERE IDMONEDA_1 = 2 ORDER BY FECHAHORAACTUALIZACION DESC LIMIT 1")
