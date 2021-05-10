2735662625f86b35ada81493f2e3a81fa9932fc7
royersynopsis <royerleandroroblesvega@gmail.com>
2021-05-10 00:47:00 -0500

    db

--- weexConstants.py
+++ weexConstants.py
@@ -1,14 +1,14 @@
 
-# MYSQL_HOST = 'localhost'
-# MYSQL_USER = 'weex_admirasite'
-# MYSQL_PASSWORD = 'weex_admirasite123.'
-# MYSQL_DB = 'weex_admira' 
+MYSQL_HOST = 'localhost'
+MYSQL_USER = 'weex_admirasite'
+MYSQL_PASSWORD = 'weex_admirasite123.'
+MYSQL_DB = 'weex_admira' 
 
 
 API_ACTUALIZAR_TIPO_CAMBIO = 'http://demo.weex.pe/weex/actualizar/tasa-cambio/v1'
 #API_ACTUALIZAR_TIPO_CAMBIO = 'http://localhost:5000/weex/actualizar/tasa-cambio/v1'
 
-MYSQL_HOST = 'localhost'
-MYSQL_USER = 'root'
-MYSQL_PASSWORD = 'admin'
-MYSQL_DB = 'weex_admira'
+# MYSQL_HOST = 'localhost'
+# MYSQL_USER = 'root'
+# MYSQL_PASSWORD = 'admin'
+# MYSQL_DB = 'weex_admira'
